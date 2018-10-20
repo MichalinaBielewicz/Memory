4729293dd4e005fb40e02841269aedeba86c7e03
MichalinaBielewicz <micbiel2@student.pg.gda.pl>
2018-10-20 18:17:06 +0200

    Dodanie 4 przycisków

--- memory.py
+++ memory.py
@@ -41,8 +41,8 @@ def click(przyciski, txt,lista, id):
     button.pack()
 
 # losowanie pozycji
-pozycja=[[0,0],[0,1],[0,2],[0,3],[1,0],[1,1],[1,2],[1,3]]
-pomocny= [0,1,2,3,4,5,6,7]
+pozycja=[[0,0],[0,1],[0,2],[0,3],[1,0],[1,1],[1,2],[1,3],[2,0],[2,1],[2,2],[2,3]]
+pomocny= [0,1,2,3,4,5,6,7,8,9,10,11]
 pomocny = random.sample(pomocny, k=len(pomocny))
 
 przyciski=[]
@@ -63,6 +63,14 @@ b7 = Button(master, text=" ", command=lambda: click(przyciski, "Hi", lista, 7), 
 b7.grid(row=pozycja[pomocny[6]][0], column=pozycja[pomocny[6]][1])
 b8 = Button(master, text=" ", command=lambda: click(przyciski, "Hi", lista, 8), background="skyblue", width=8, height=5, state=NORMAL)
 b8.grid(row=pozycja[pomocny[7]][0], column=pozycja[pomocny[7]][1])
+b9 = Button(master, text=" ", command=lambda: click(przyciski, "Thank you", lista, 9), background="skyblue", width=8, height=5, state=NORMAL)
+b9.grid(row=pozycja[pomocny[8]][0], column=pozycja[pomocny[8]][1])
+b10 = Button(master, text=" ", command=lambda: click(przyciski, "Thank you", lista, 10), background="skyblue", width=8, height=5, state=NORMAL)
+b10.grid(row=pozycja[pomocny[9]][0], column=pozycja[pomocny[9]][1])
+b11 = Button(master, text=" ", command=lambda: click(przyciski, "I am Kuba", lista, 11), background="skyblue", width=8, height=5, state=NORMAL)
+b11.grid(row=pozycja[pomocny[10]][0], column=pozycja[pomocny[10]][1])
+b12 = Button(master, text=" ", command=lambda: click(przyciski, "I am Kuba", lista, 12), background="skyblue", width=8, height=5, state=NORMAL)
+b12.grid(row=pozycja[pomocny[11]][0], column=pozycja[pomocny[11]][1])
 
 przyciski.append(b1)
 przyciski.append(b2)
@@ -72,6 +80,10 @@ przyciski.append(b5)
 przyciski.append(b6)
 przyciski.append(b7)
 przyciski.append(b8)
+przyciski.append(b9)
+przyciski.append(b10)
+przyciski.append(b11)
+przyciski.append(b12)
 
 # poniższe trzy linijki zastępują "mainloop()", a po nich następuje kod dodatkowy
 while True:
